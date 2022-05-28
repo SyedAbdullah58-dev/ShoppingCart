40b0329dbe4ee26ae128c51f5e3968d049b9394a
SyedAbdullah-harrisERP <SAbdullah@harriscomputer.com>
2022-05-28 19:01:46 +0500

    Add project files.

---
+++ ShoppingCart/Data/ShoppingContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingCart.Data
+{
+    public class ShoppingContext:DbContext
+
+    {
+        public ShoppingContext(DbContextOptions<ShoppingContext> options):base(options)
+        {
+
+        }
+
+    }
+}
